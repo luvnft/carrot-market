4dab70e2030f4cdc0a31ff9ccd54091e05e007e7
lbo728 <extreme93@naver.com>
2023-11-20 10:31:20 +0900

    8.3 Clean Code part Two

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -13,7 +13,7 @@ interface EnterForm {
 }
 
 const Enter: NextPage = () => { 
-  const [enter, {loading, data, error}] = useMutation();
+  const [enter, {loading, data, error}] = useMutation("/api/users/enter");
   const [submitting, setSubmitting] = useState(false);
   const { register, watch, reset, handleSubmit } = useForm<EnterForm>();
   const [method, setMethod] = useState<"email" | "phone">("email");
@@ -25,18 +25,10 @@ const Enter: NextPage = () => {
     reset(); 
     setMethod("phone");
   }
-  const onValid = (data: EnterForm) => {
-    setSubmitting(true);
-    fetch("/api/users/enter", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Tpye": "application/json"
-      }
-    }).then(() => {
-      setSubmitting(false);
-    })
+  const onValid = (validForm: EnterForm) => {
+    enter(validForm);
   };
+  console.log(loading, data, error);
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
