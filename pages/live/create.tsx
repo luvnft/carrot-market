1ec6befeaa3480127abd6a0edd0aef97dd5dc57e
luvnft <140175751+luvnft@users.noreply.github.com>
2023-11-25 11:11:14 -0500

    Update create.tsx

--- pages/live/create.tsx
+++ pages/live/create.tsx
@@ -1,15 +1,20 @@
-import type { NextPage } from "next";
-import Button from "@components/button";
-import Input from "@components/input";
-import Layout from "@components/layout";
-import TextArea from "@components/textarea";
+import React from 'react';
+import type { NextPage } from 'next';
+import Button from '@components/button';
+import Input, { InputProps } from '@components/input';
+import Layout from '@components/layout';
+import TextArea from '@components/textarea';
+
+const CustomInput: React.FC<InputProps> = ({ register, ...otherProps }) => {
+  return <input ref={register} {...otherProps} />;
+};
 
 const Create: NextPage = () => {
   return (
     <Layout canGoBack title="Go Live">
-      <form className=" space-y-4 py-10 px-4">
-        <Input required label="Name" name="name" type="text" />
-        <Input
+      <form className="space-y-4 py-10 px-4">
+        <CustomInput required label="Name" name="name" type="text" />
+        <CustomInput
           required
           label="Price"
           placeholder="0.00"
